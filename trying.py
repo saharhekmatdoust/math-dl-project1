2c3ae3c3dfb1cb51869b182923cbe330423d5318
Sahar Hekmatdoust <69188604+saharhekmatdoust@users.noreply.github.com>
2023-03-28 11:58:28 -0400

    trying

--- trying.py
+++ trying.py
@@ -1,46 +1,57 @@
 import streamlit as st
 import pandas as pd
-import os
-import time
 import numpy as np
-import pandas as pd
-from collections import namedtuple
-from utils import openai_auth
-from openai.embeddings_utils import get_embedding
-from openai.embeddings_utils import cosine_similarity
-import tiktoken
+from sentence_transformers import SentenceTransformer
+import ast
+#import spacy
 
-st.title("Looking for a job title description")
-
-# adding a single-line text input widget
-query = st.text_input('Job title')
-
-st.write('You requested job description for job title: ', query)
-
-DOCUMENTS_EMBEDDINGS_PATH = "data"  # a folder with all the documents embeddings. within this folder, one csv file include multiple documents embedding of the same run
-COLUMN_EMBEDDINGS = "embedding"  # the embedding column name in the documents embedding file.
-
-file_path = "data/TaskEmbeddingss.csv"
-EMBEDDING_MODEL = "text-embedding-ada-002"
+# Load a pre-trained model for word embeddings
+model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+# Load your dataframe here
+file_path_mpnet = "data/top500embeddings.csv.csv"
+df_mpnet= pd.read_csv(file_path_mpnet)
+df_mpnet['embeddings '] = df_mpnet['embeddings '].apply(ast.literal_eval)
 
 
-# read documents embeddings
-df = pd.read_csv(file_path)
-df[COLUMN_EMBEDDINGS] = df[COLUMN_EMBEDDINGS].apply(eval).apply(np.array)  # convert string to np array
-st.write(f'Read {len(df)} documents embeddings from {file_path}')
+# Define a function to calculate the cosine similarity between two vectors
+def cosine_similarity_mpnet(a,b):
+    dot_product = np.dot(a, b)
+    magnitude_a = np.sqrt(np.dot(a, a))
+    magnitude_b = np.sqrt(np.dot(b, b))
+    cos_sim = dot_product / (magnitude_a * magnitude_b)
+    return cos_sim
 
-openai_auth()
+# Define the Streamlit app
+def app():
+    # Add a title and description
+    st.title("Job Responsibility Finder")
+    st.write("Enter a Job Title and get The required Tasks for it.")
 
-while True:
-    query = st.text_input('Please enter a job title or type "exit" to exit')
-    if query == "exit":
-        break
-    with st.spinner('Computing embeddings...'):
-        tic = time.time()
-        query_embedding = get_embedding(query, engine=EMBEDDING_MODEL)
-        toc = time.time()
-    st.write(f'Embedding query took {round(toc-tic)*1000}ms')
-    st.write('Top matches ordered by cosine similarity of vector embeddings:')
-    df['similarity'] = df[COLUMN_EMBEDDINGS].apply(lambda x: cosine_similarity(x, query_embedding))
-    result = df.sort_values(by='similarity', ascending=False)[['similarity', 'description']].head(10)
-    st.write(result)
+    # Add a text input for the user to enter their text
+    input_text = st.text_input("Enter a Job Title")
+
+    # Check if the user has entered any text
+    if input_text:
+        # Process the input text to get its embedding
+        input_vector = model.encode(input_text) 
+
+        # Calculate the cosine similarity between the input vector and all the vectors in the dataframe
+        similarity_scores =  [cosine_similarity_mpnet(input_vector,embedding) for embedding in df_mpnet['embeddings '].tolist()]
+
+        # Get the top 10 most similar rows 
+        #top_10_indices = similarity_scores.argsort()[::-1][:10]
+        top_10_indices = np.argsort(-np.array(similarity_scores))[:5]
+        selected_rows =df_mpnet.loc[top_10_indices.tolist(), 'Task']
+        #top_10_scores = similarity_scores.iloc[top_5_indices]
+        #top_10_rows =  df_mpnet.iloc[top_5_indices]
+        for i,desc in enumerate(selected_rows):
+            st.write("Job Responsibility Number",i+1,'is:\n',desc)
+
+        # Display the top 5 most similar rows
+        #st.write("Top 5 most similar rows:")
+        #for i, row in top_5_rows.iterrows():
+         #   st.write(f"{i}. {row['Task']} (similarity score: {top_5_scores.loc[i]:.2f})")
+
+# Run the app
+if __name__ == "__main__":
+    app()
